9089cf6530a388f59efa8999a35d735148c7a4bc
cuvar <luca-mueller@gmx.net>
2021-11-16 10:51:44 +0100

    fix: lifted state to config component

--- src/components/Config/ColorPicker/index.tsx
+++ src/components/Config/ColorPicker/index.tsx
@@ -1,26 +1,42 @@
 import { SketchPicker } from 'react-color';
 import { useState } from 'react';
 interface IProps {
-    className?: string;
-    name: string;
+  className?: string;
+  name: string;
+  onValueChange: (value: string) => void;
 }
-  
+
 function ColorPicker(props: IProps) {
-    const [displayColorPicker, setDisplayColorPicker] = useState(false);
-    const [color, setColor] = useState("#0C7ECF");
-    return (    
-        <div className={props.className + ' '}>    
-            <label className='float-left pr-4'>{props.name + ':'}</label>
-            <button className='float-right px-8 py-3' style={{background: color}}  onClick={() => setDisplayColorPicker(!displayColorPicker) }></button>
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+  const [color, setColor] = useState('#0C7ECF');
 
-            {displayColorPicker ? <div className='absolute'>
-            <div className='fixed' onClick={() => setDisplayColorPicker(!displayColorPicker) }/>
-            <SketchPicker disableAlpha className='' color={color} onChange={(color) => {setColor(color.hex)}} onChangeComplete={(color) => {setColor(color.hex)}}/>
-            </div>
-            : null }        
+  function handler(event: any) {
+    setColor(event);
+    props.onValueChange(event);
+  }
+  return (
+    <div className={props.className + ' '}>
+      <label className='float-left pr-4'>{props.name + ':'}</label>
+      <button className='float-right px-8 py-3' style={{ background: color }} onClick={() => setDisplayColorPicker(!displayColorPicker)}></button>
+
+      {displayColorPicker ? (
+        <div className='absolute'>
+          <div className='fixed' onClick={() => setDisplayColorPicker(!displayColorPicker)} />
+          <SketchPicker
+            disableAlpha
+            className=''
+            color={color}
+            onChange={(color) => {
+              handler(color.hex);
+            }}
+            onChangeComplete={(color) => {
+              handler(color.hex);
+            }}
+          />
         </div>
-    );
-
+      ) : null}
+    </div>
+  );
 }
-  
-export default ColorPicker;  
+
+export default ColorPicker;
--- src/components/Config/ValueFieldInput/index.tsx
+++ src/components/Config/ValueFieldInput/index.tsx
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 
 interface IProps {
-    className?: string;
-    name: string;
+  className?: string;
+  name: string;
+  onValueChange: (value: string) => void;
 }
-  
+
 function ValueFieldInput(props: IProps) {
-    const [value, setValue] = useState('');
-    return (
-        <div  className={props.className + ' ' }>
-            <label className='pr-4 float-left'>{props.name + ':'}</label>
-            <input placeholder="..." className='float-right px-2 w-20' onChange={(event) => (setValue(event.target.value))}/>
-        </div>
-    );
+  const [value, setValue] = useState('');
+
+  function handler(event: any) {
+    setValue(event.target.value);
+    props.onValueChange(event.target.value);
+  }
+
+  return (
+    <div className={props.className + ' '}>
+      <label className='pr-4 float-left'>{props.name + ':'}</label>
+      <input placeholder='...' className='float-right px-2 w-20' onChange={(event) => handler(event)} />
+    </div>
+  );
 }
-  
-export default ValueFieldInput;  
+
+export default ValueFieldInput;
