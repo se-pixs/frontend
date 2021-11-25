ed238d051367dbb0f3b1ca7b3bda814a6c2f905e
Paul Ehrhard <82864530+paul910@users.noreply.github.com>
2021-11-25 13:33:17 +0100

    value bug fix

--- src/components/Start/index.tsx
+++ src/components/Start/index.tsx
@@ -9,9 +9,10 @@ import Title from '../Title';
 import Config from '../Config';
 import Spacer from '../Spacer';
 import Preview from '../Preview';
-
-import backend from '../../example.json'
 import ProgressBar from '../ProgressBar';
+
+import backend from '../../example.json';
+import def from '../../default.json';
 
 interface actionObject {
   name: string;
@@ -39,7 +40,10 @@ function Start() {
   }
 
   function onActionChange(name: string){
-    setActionName(name);
+    setActionName("");
+    setTimeout(function(){
+      setActionName(name);
+    },1)
   }
 
   let UPLOADED: boolean = true;
@@ -53,7 +57,7 @@ function Start() {
         <Header />
         <div className='bg-customwhite-500 flex flex-col justify-between px-40 py-20'>
           <ProgressBar/>
-          <Title title={actionName.toUpperCase()} description={backend.actions.filter((action) => (action.name === actionName))[0].description} />
+          <Title title={actionName.toUpperCase()} description={actionName !== "" ? backend.actions.filter((action) => (action.name === actionName))[0].description : ""} />
           <UploadField />
           <Spacer />
           <Config runAction={runAction} uploaded={UPLOADED} configList={configList} />
@@ -99,8 +103,9 @@ function runAction(event: any){
 }
 
 function getActionList(actionName: string): any {
-  const defaultActive: boolean = false;
-
+  if(actionName === ""){
+    return JSON.parse(JSON.stringify(def.actions));
+  }
   const defaultIcon: React.ReactNode = (
     <svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='crop-alt' className='w-8 h-8' role='img' xmlns='http://www.w3.org/2000/svg' fill='currentColor' stroke='none' viewBox='0 0 512 512'>
       <path d='M488 352h-40V96c0-17.67-14.33-32-32-32H192v96h160v328c0 13.25 10.75 24 24 24h48c13.25 0 24-10.75 24-24v-40h40c13.25 0 24-10.75 24-24v-48c0-13.26-10.75-24-24-24zM160 24c0-13.26-10.75-24-24-24H88C74.75 0 64 10.74 64 24v40H24C10.75 64 0 74.74 0 88v48c0 13.25 10.75 24 24 24h40v256c0 17.67 14.33 32 32 32h224v-96H160V24z'></path>
@@ -121,6 +126,9 @@ function getActionList(actionName: string): any {
 }
 
 function getConfigList(actionName: string): any {
+  if(actionName === ""){
+    return JSON.parse(JSON.stringify(def.actions));
+  }
   return JSON.parse(JSON.stringify(backend.actions.filter((action) => (action.name === actionName))[0]));
 }
 
