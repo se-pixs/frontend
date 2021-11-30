800a8702dcba2ec9caff3525aaa7e676329984f5
cuvar <luca-mueller@gmx.net>
2021-11-30 17:55:31 +0100

    fix: config design

--- components/Config/ColorPicker/index.tsx
+++ components/Config/ColorPicker/index.tsx
@@ -11,22 +11,25 @@ interface IProps {
 
 function ColorPicker(props: IProps) {
   const [displayColorPicker, setDisplayColorPicker] = useState(false);
-  const [color, setColor] = useState("#" + ((1 << 24) + (props.defaultColor[0] << 16) + (props.defaultColor[1] << 8) + props.defaultColor[2]).toString(16).slice(1));
+  const [color, setColor] = useState('#' + ((1 << 24) + (props.defaultColor[0] << 16) + (props.defaultColor[1] << 8) + props.defaultColor[2]).toString(16).slice(1));
 
   function handler(event: any) {
     setColor(event);
     props.onValueChange(event, props.name);
   }
+
+  const actionNameToUpper: string = props.name.charAt(0).toUpperCase() + props.name.slice(1);
+
   return (
-    <div className={props.className + ' '}>
-      <label className='float-left pr-2'>{props.name + ':'}</label>
-      <button className='float-right px-10 py-4 rounded-md' style={{ background: color }} onClick={() => setDisplayColorPicker(!displayColorPicker)}></button>
+    <div className={props.className + ' flex items-center'}>
+      <label className='mr-4'>{actionNameToUpper}</label>
+      <button className='px-10 py-4 rounded-md' style={{ background: color }} onClick={() => setDisplayColorPicker(!displayColorPicker)}></button>
       {displayColorPicker && (
         <div className='absolute'>
           <div className='fixed' onClick={() => setDisplayColorPicker(!displayColorPicker)} />
           <div style={{ background: color }} className='w-full text-right rounded-t-md flex justify-between'>
-            <p className='text-left px-4 py-2 flex-initial font-bold'>{props.name}</p>
-            <button onClick={() => setDisplayColorPicker(!displayColorPicker)} className='flex-initial bg-custompurple-200 border-2 border-custompurple-500 hover:bg-custompurple-400 text-black py-1 px-4 text-right rounded-md'>
+            <p className='text-left px-4 py-2 flex-initial font-bold'>{actionNameToUpper}</p>
+            <button onClick={() => setDisplayColorPicker(!displayColorPicker)} className='flex-initial bg-white hover:bg-custompurple-100 text-black py-1 px-4 text-right '>
               Close
             </button>
           </div>
