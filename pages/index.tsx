cc34261d6e114df34859931fad6372421eadd89c
cuvar <luca-mueller@gmx.net>
2021-12-03 18:51:04 +0100

    feat: added abillity to set session cookie

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,23 @@ import Head from 'next/head';
 import Start from '../components/Start';
 import pixsConfig from '../pixs.config.json';
 import { actionObject } from '../components/SideBar/types';
+import Axios from 'axios';
+// import cookieCutter from 'cookie-cutter';
 
 interface IProps {
   actionsList: actionObject[];
+  cookie: string;
 }
 
 const Home: NextPage<IProps> = (props: IProps) => {
+  const error: boolean = props.cookie === '';
+
+  if (typeof document !== 'undefined') {
+    document.cookie = props.cookie;
+    console.log(props.cookie);
+    console.log(document.cookie);
+  }
+
   return (
     <div>
       <Head>
@@ -16,7 +27,8 @@ const Home: NextPage<IProps> = (props: IProps) => {
         <meta name='description' content='PiXS - Image Manipulation Extended' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Start actionsList={props.actionsList} />
+      {!error && <Start actionsList={props.actionsList} />}
+      {error && <div>An error occured. Please try reloading the page</div>}
     </div>
   );
 };
@@ -25,13 +37,15 @@ export default Home;
 
 export async function getServerSideProps() {
   let actionsListTemp: actionObject[] = [];
-  let activeActionNameTemp = '';
+  // let activeActionNameTemp = '';
+  let customCookie: string = '';
 
   try {
-    const data = await fetch(pixsConfig.backend);
-    const jsonData = await data.json();
+    // const data = await fetch(pixsConfig.backend);
+    const response = await Axios.get(pixsConfig.backend);
 
-    actionsListTemp = jsonData.actions.slice(2);
+    actionsListTemp = response.data.actions.slice(2);
+    customCookie = response.headers['set-cookie'] ? response.headers['set-cookie'][0] : '';
   } catch (e) {
     console.log(e);
   }
@@ -44,6 +58,7 @@ export async function getServerSideProps() {
   return {
     props: {
       actionsList: actionsListTemp,
+      cookie: customCookie,
     },
   };
 }
