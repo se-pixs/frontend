58aedc179718f8c0db5844ab9c407a5b42a6a646
cuvar <luca-mueller@gmx.net>
2021-12-03 10:52:47 +0100

    fix: added text contrast color for color picker

--- components/Config/ColorPicker/index.tsx
+++ components/Config/ColorPicker/index.tsx
@@ -1,5 +1,6 @@
 import { SketchPicker } from 'react-color';
 import { useState } from 'react';
+import { getContrastColor } from '../../../util/color';
 
 interface IProps {
   className?: string;
@@ -12,10 +13,13 @@ interface IProps {
 function ColorPicker(props: IProps) {
   const [displayColorPicker, setDisplayColorPicker] = useState(false);
   const [color, setColor] = useState('#' + ((1 << 24) + (props.defaultColor[0] << 16) + (props.defaultColor[1] << 8) + props.defaultColor[2]).toString(16).slice(1));
+  const [textColor, setTextColor] = useState(getContrastColor(color));
 
   function handler(event: any) {
     setColor(event);
     props.onValueChange(event, props.name);
+    setTextColor(getContrastColor(color));
+    console.log(textColor);
   }
 
   const actionNameToUpper: string = props.name.charAt(0).toUpperCase() + props.name.slice(1);
@@ -28,7 +32,9 @@ function ColorPicker(props: IProps) {
         <div className='absolute'>
           <div className='fixed' onClick={() => setDisplayColorPicker(!displayColorPicker)} />
           <div style={{ background: color }} className='w-full text-right rounded-t-md flex justify-between'>
-            <p className='text-left px-4 py-2 flex-initial font-bold'>{actionNameToUpper}</p>
+            <p className='text-left px-4 py-2 flex-initial font-bold' style={{ color: textColor }}>
+              {actionNameToUpper}
+            </p>
             <button onClick={() => setDisplayColorPicker(!displayColorPicker)} className='flex-initial bg-white hover:bg-custompurple-100 text-black py-1 px-4 text-right '>
               Close
             </button>
