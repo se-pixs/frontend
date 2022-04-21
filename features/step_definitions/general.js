0393e78681f3191e3a0a8a381dac7df9bf6fa5f3
Luca Müller <49287189+cuvar@users.noreply.github.com>
2022-04-21 08:41:09 +0200

    fix: add explanation for single like step def

--- features/step_definitions/general.js
+++ features/step_definitions/general.js
@@ -1,6 +1,11 @@
 const assert = require('assert');
 const { Then } = require('@cucumber/cucumber');
 
+// Note: this file only contains a single statement as it is used to 
+// out source statements that are used more than once for the use cases.
+// Thereofore, changes can be made at a single point in code, rather 
+// than at various lines in the cucumber code. 
+
 Then('user should see a {string}', function (expectedAnswer) {
   assert.equal(this.actualAnswer, expectedAnswer);
 });
