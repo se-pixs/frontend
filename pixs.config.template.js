b101aa58f4e8dd6cb6d5557c6eb256b40d1b6184
cuvar <luca-mueller@gmx.net>
2022-04-25 16:56:11 +0200

    fix: gitignore

---
+++ pixs.config.template.js
@@ -0,0 +1,13 @@
+module.exports = {
+  backend: {
+    api: getPropertyOrUseDefault(process.env.BACKEND_API_URL, "http://localhost:8000"),
+    resources: getPropertyOrUseDefault(process.env.BACKEND_RESOURCES_URL, "http://localhost:8000"),
+  },
+  frontend: "http://localhost:3000",
+  iconPlaceholder: "/icon-placeholder.svg",
+  useIPv6: false
+}
+
+function getPropertyOrUseDefault(property, defaultValue) {
+  return property ? property : defaultValue;
+}
