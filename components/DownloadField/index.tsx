f96ec4f002691f0b017fde650fb04106c69e0660
cuvar <luca-mueller@gmx.net>
2022-05-19 15:26:12 +0200

    feat: outsource modal

--- components/DownloadField/index.tsx
+++ components/DownloadField/index.tsx
@@ -4,6 +4,7 @@ import useStore from '../../utils/store/globalStore';
 
 import Button from '../Button';
 import BackgroundBlur from '../BackgroundBlur';
+import DeleteModal from '../Modal/deleteModal';
 
 interface IProps {
   className?: string;
@@ -31,36 +32,13 @@ function DownloadField(props: IProps) {
   }
 
   function deleteAndRetry() {
+    setShowDeleteModal(false);
     props.deleteAndRetry();
   }
 
   return (
     <div className={'flex flex-col' + ' ' + props.className}>
-      {showDeleteModal && (
-        <BackgroundBlur>
-          <div className='bg-white p-5 rounded-lg'>
-            <h2 className='text-xl font-bold'>Delete and retry</h2>
-            <hr className='mb-4 border-gray-300' />
-            <div>
-              <p className='text-lg px-2'>Do you really want to delete your image manipulation and retry?</p>
-              <div className='w-full flex justify-evenly'>
-                <Button
-                  className='mt-4'
-                  onclick={() => {
-                    setShowDeleteModal(false);
-                    deleteAndRetry();
-                  }}
-                  disabled={false}>
-                  Yes
-                </Button>
-                <Button className='mt-4' onclick={() => setShowDeleteModal(false)} disabled={false} type={'error'}>
-                  No
-                </Button>
-              </div>
-            </div>
-          </div>
-        </BackgroundBlur>
-      )}
+      {showDeleteModal && <DeleteModal show={false} deleteAndRetry={deleteAndRetry} showDeleteModal={(showModal: boolean) => setShowDeleteModal(showModal)} />}
       <h2 className='text-2xl font-bold text-customblue-200 mb-5 text-center'>Your edited image</h2>
       <div className='flex justify-center mb-5'>
         <img src={props.imageData} alt='Downloaded' height='fill' width='fill' />
