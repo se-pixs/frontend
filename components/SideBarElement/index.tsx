0c03063f056e897ca8d640aac0d82dd23f1b49ab
cuvar <luca-mueller@gmx.net>
2022-06-18 13:08:08 +0200

    fix: size of icons in sidebar

--- components/SideBarElement/index.tsx
+++ components/SideBarElement/index.tsx
@@ -11,7 +11,7 @@ function SideBarElement(props: IProps) {
   return (
     <div className={'text-customwhite-500 h-14 w-full text-lg flex flex-initial items-center hover:bg-customblue-200 cursor-pointer' + (props.active ? ' ' + activeClass : '')}>
       <div className={'w-1 h-full' + (props.active ? ' bg-custompurple-500' : ' bg-transparent')} />
-      <img className='flex-initial ml-4 mr-4 text-customwhite-500 fill-current' src={props.icon} width='50' height='50' />
+      <img className='flex-initial ml-4 mr-4 text-customwhite-500 fill-current' src={props.icon} width='30' height='30' />
       {props.isBig && <p className='flex-initial ml-4 mr-6 whitespace-nowrap'>{props.displayName}</p>}
     </div>
   );
